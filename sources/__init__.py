4e7e65ec36cd8a986221097d0790cc6f5e73a08f
cofinets <Rezaeemajd2008@outlook.com>
2026-09-20 16:19:29 +0330

    Add source adapters package

---
+++ sources/__init__.py
@@ -0,0 +1,6 @@
+from .base import BaseAdapter, ProductCandidate
+from .mofidteb import MofidTebAdapter
+from .daroohome import DaroohomeAdapter
+from .wordpress import WordPressAdapter
+
+__all__ = ["BaseAdapter", "ProductCandidate", "MofidTebAdapter", "DaroohomeAdapter", "WordPressAdapter"]
